c8860dc75a3b8eb96c87011a6fcf3847d61cf880
Isabelascolla <102077830+Isabelascolla@users.noreply.github.com>
2022-12-15 18:57:04 -0300

    Add files via upload

---
+++ #batalha de pokemon.py
@@ -0,0 +1,17 @@
+#batalha de pokemon
+
+import pandas as pd
+from classe_pokemon import pokemon
+
+
+pokemon=pd.read_csv("pokemon_data.csv")
+
+poke1_HP=pokemons.iloc[[1]]
+
+def _int_(self,numero,nome,tipo1,tipo2,hp,atk,defesa,sp_atk,sp_atk,sp_def,speed,gem,len)
+l=[]
+for i in range(len(pokemons.columns)):
+    l.append(poke1.iloc[1,i])
+print(1)
+
+pokemon1=pokemon(*1)
